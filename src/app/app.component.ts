129051343367f0d16f7b0a7431207257631bfa1a
Graeme <graemenorman@tengroup.com>
2018-06-21 16:50:39 +0100

    - environment config pulled into component and properties output to template file (as a test!)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  // Declarations
+  title: string;
+  envName: string;
+
+  ngOnInit() {
+    this.title = 'Welcome to the TFL mobile app';
+
+    this.envName = environment.name;
+
+    // console.log('My environment name is ', environment.name );
+
+  }
+
 }
